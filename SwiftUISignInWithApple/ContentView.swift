888b1474078feff9df6c4dea16c780522d90ac2e
Alex Nagy <therebeloper@gmail.com>
2019-11-04 20:18:35 +0200

    Initial Commit

---
+++ SwiftUISignInWithApple/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUISignInWithApple
+//
+//  Created by Alex Nagy on 04/11/2019.
+//  Copyright © 2019 Alex Nagy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
